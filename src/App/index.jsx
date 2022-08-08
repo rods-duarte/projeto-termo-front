8597a70e2ace4edac8344a61997262fc4aa8d6d8
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-08-08 16:22:21 -0300

    Feat: handle key press

---
+++ src/App/index.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import GameBoard from "../components/GameBoard";
+
+const wordLength = 5; //! .env
+
+export default function App() {
+  const [attempt, setAttempt] = useState("");
+  const [turn, setTurn] = useState(0);
+  const [guesses, setGuesses] = useState([...Array(6)]);
+  const [letterUsed, setLettersUsed] = useState({});
+  const answer = "teste"; //eslint-disable-line
+
+  function handleKeyUp({ key }) {
+    console.log(key);
+
+    if (key === "Enter" && attempt.length === 5) {
+      // TODO verify answer
+      if (attempt === answer) {
+        alert("certa resposta");
+      }
+    }
+
+    if (key === "Backspace") {
+      setAttempt((state) => state.slice(0, -1));
+    }
+
+    if (/^[A-Za-z]$/.test(key)) {
+      if (attempt.length < wordLength) {
+        setAttempt((state) => state + key);
+      }
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyUp);
+    return () => window.removeEventListener("keyup", handleKeyUp);
+  }, [handleKeyUp]);
+
+  return <div />;
+}

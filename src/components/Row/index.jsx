dafeb441c48eb8fe549bb4432add95d7b1ba9af1
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-08-08 16:24:48 -0300

    Feat: add word row component

---
+++ src/components/Row/index.jsx
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+const wordLength = 5;
+export default function Row({ guess, attempt }) {
+  if (guess) {
+    return (
+      <WordRow>
+        {guess.map((letter) => (
+          <LetterBox>{letter.toUpperCase()}</LetterBox>
+        ))}
+      </WordRow>
+    );
+  }
+
+  if (attempt) {
+    const emptySpaces = wordLength - attempt.length;
+    const emptyBoxes = [...Array(emptySpaces)];
+    return (
+      <WordRow>
+        {attempt.split("").map((letter) => (
+          <LetterBox>{letter.toUpperCase()}</LetterBox>
+        ))}
+        {emptyBoxes.map(() => (
+          <LetterBox />
+        ))}
+      </WordRow>
+    );
+  }
+
+  return (
+    <WordRow>
+      <LetterBox> </LetterBox>
+      <LetterBox> </LetterBox>
+      <LetterBox> </LetterBox>
+      <LetterBox> </LetterBox>
+      <LetterBox> </LetterBox>
+    </WordRow>
+  );
+}
+
+const WordRow = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 10px 0;
+`;
+
+const LetterBox = styled.div`
+  width: 80px;
+  height: 80px;
+  border: 1px solid #333;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 50px;
+`;

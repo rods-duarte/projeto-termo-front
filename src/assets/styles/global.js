0506e79e6e3507a1752c4a02007af395ddf37d8e
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-08-10 01:40:33 -0300

    Feat: add Mitr font family

--- src/assets/styles/global.js
+++ src/assets/styles/global.js
@@ -5,5 +5,10 @@ export default createGlobalStyle`
         box-sizing: border-box;
         margin: 0;
         padding: 0;
+        font-family: 'Mitr', sans-serif;
+    }
+
+    #root {
+        background-color: #121213;
     }
 `;

22a8d60d90aa2ea7aecdb584417f9aa995b452d1
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-08-10 02:21:18 -0300

    Feat: add styles to keyboard component

--- src/components/Keyboard/index.jsx
+++ src/components/Keyboard/index.jsx
@@ -2,41 +2,45 @@ import { useState } from "react";
 import styled from "styled-components";
 
 export default function Keyboard({ lettersUsed }) {
+  console.log(
+    "🚀 ~ file: index.jsx ~ line 5 ~ Keyboard ~ lettersUsed",
+    lettersUsed
+  );
   const [letters, setLetters] = useState([
-    { key: "q" },
-    { key: "w" },
-    { key: "e" },
-    { key: "r" },
-    { key: "t" },
-    { key: "y" },
-    { key: "u" },
-    { key: "i" },
-    { key: "o" },
-    { key: "p" },
-    { key: "a" },
-    { key: "s" },
-    { key: "d" },
-    { key: "f" },
-    { key: "g" },
-    { key: "h" },
-    { key: "j" },
-    { key: "k" },
-    { key: "l" },
-    { key: "z" },
-    { key: "x" },
-    { key: "c" },
-    { key: "v" },
-    { key: "b" },
-    { key: "n" },
-    { key: "m" },
+    { key: "Q" },
+    { key: "W" },
+    { key: "E" },
+    { key: "R" },
+    { key: "T" },
+    { key: "Y" },
+    { key: "U" },
+    { key: "I" },
+    { key: "O" },
+    { key: "P" },
+    { key: "A" },
+    { key: "S" },
+    { key: "D" },
+    { key: "F" },
+    { key: "G" },
+    { key: "H" },
+    { key: "J" },
+    { key: "K" },
+    { key: "L" },
+    { key: "Z" },
+    { key: "X" },
+    { key: "C" },
+    { key: "V" },
+    { key: "B" },
+    { key: "N" },
+    { key: "M" },
   ]);
 
   const firstRow = [];
   const secondRow = [];
   const thirdRow = [];
 
-  const keys = letters.forEach((letter, index) => {
-    const color = lettersUsed[letter.key]?.color;
+  letters.forEach((letter, index) => {
+    const color = lettersUsed[letter.key.toLowerCase()]?.color;
     if (index <= 9) {
       firstRow.push(<Key className={color}>{letter.key}</Key>);
     } else if (index <= 18) {
@@ -61,11 +65,14 @@ const KeyboardContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  row-gap: 5px;
+  font-weight: 500;
 
   & > div {
     width: fit-content;
     display: flex;
     justify-content: center;
+    column-gap: 2px;
   }
 `;
 
@@ -74,24 +81,22 @@ const Key = styled.div`
   height: 43px;
   margin: 1px;
   border-radius: 5px;
-  background-color: antiquewhite;
+  background-color: #818384;
+  color: #fff;
 
   display: flex;
   justify-content: center;
   align-items: center;
 
   &.grey {
-    background-color: #999;
-    color: #fff;
+    background-color: #3a3a3c;
   }
 
   &.yellow {
-    background-color: #d08b01;
-    color: #fff;
+    background-color: #b59f3b;
   }
 
   &.green {
-    background-color: #28af28;
-    color: #fff;
+    background-color: #538d4e;
   }
 `;

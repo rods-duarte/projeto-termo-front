9cf7897933579d1cd0709b155d4a170b58a224a9
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-08-16 16:55:26 -0300

    Fix: api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseAPI = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://app-termo.herokuapp.com/",
 });
 
 function getConfig(token) {

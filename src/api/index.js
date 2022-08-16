7bc857a45c441efa4cfa68d41d591fdcf2a1a7da
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-08-16 12:21:00 -0300

    Feat: add api

---
+++ src/api/index.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const baseAPI = axios.create({
+  baseURL: "http://localhost:5000/",
+});
+
+function getConfig(token) {
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+}
+
+async function signup(signupData) {
+  await baseAPI.post("/signup", signupData);
+}
+
+async function signin(signinData) {
+  return baseAPI.post("/signin", signinData);
+}
+
+async function updateStats(id, token, stats) {
+  return baseAPI.post(`/users/${id}`, stats, getConfig(token));
+}
+
+async function getGameData() {
+  const response = await baseAPI.get("/game");
+  return response.data;
+}
+
+export const api = {
+  getConfig,
+  signup,
+  signin,
+  getGameData,
+  updateStats,
+};

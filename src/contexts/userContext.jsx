77cc8b77ee03bc05f9c87c331bf2b916d05d95f0
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-08-16 12:24:32 -0300

    Feat: add user data context

---
+++ src/contexts/userContext.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+
+import { createContext, useState } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+export const UserContext = createContext();
+
+export function UserContextProvider({ children }) {
+  const userDataModel = {
+    wins: 0,
+    losses: 0,
+    currentStreak: 0,
+    bestStreak: 0,
+    oneGuess: 0,
+    twoGuess: 0,
+    threeGuess: 0,
+    fourGuess: 0,
+    fiveGuess: 0,
+    sixGuess: 0,
+  };
+  const [user, setUser] = useLocalStorage("user", userDataModel);
+
+  function logout() {
+    alert("Log out");
+    setUser(null);
+  }
+
+  return (
+    <UserContext.Provider value={{ user, setUser, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
